ead7cecd94b6a82bef981cb577e06f3c36b2cea9
Dylan.Wen <hhkbp2@gmail.com>
2017-03-14 16:11:10 +0800

    fix bug

--- common.sh
+++ common.sh
@@ -5,12 +5,11 @@ check_protoc_version() {
     major=$(echo ${version} | sed -n -e 's/.*\([0-9]\{1,\}\)\.[0-9]\{1,\}\.[0-9]\{1,\}.*/\1/p')
     minor=$(echo ${version} | sed -n -e 's/.*[0-9]\{1,\}\.\([0-9]\{1,\}\)\.[0-9]\{1,\}.*/\1/p')
     if [ "$major" -gt 3 ]; then
-        exit 0
+        return 0
     fi
     if [ "$major" -eq 3 ] && [ "$minor" -ge 1 ]; then
-        exit 0
+        return 0
     fi
     echo "protoc version not match, version 3.1.x is needed, current version: ${version}"
-    exit 1
+    return 1
 }
-
