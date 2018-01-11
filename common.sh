14511cd920d352d583c7d87afae3f881da422ef8
zhexuany <yangzhexuan@pingcap.com>
2018-01-11 14:24:43 +0800

    update command.sh

--- common.sh
+++ common.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 check_protoc_version() {
-    version=$(protoc --version)
-    major=$(echo ${version} | sed -n -e 's/.*\([0-9]\{1,\}\)\.[0-9]\{1,\}\.[0-9]\{1,\}.*/\1/p')
-    minor=$(echo ${version} | sed -n -e 's/.*[0-9]\{1,\}\.\([0-9]\{1,\}\)\.[0-9]\{1,\}.*/\1/p')
-    if [ "$major" -gt 3 ]; then
-        return 0
+    if [ -x "$(command -v protoc)" ]; then
+        version=$(protoc --version)
+        major=$(echo ${version} | sed -n -e 's/.*\([0-9]\{1,\}\)\.[0-9]\{1,\}\.[0-9]\{1,\}.*/\1/p')
+        minor=$(echo ${version} | sed -n -e 's/.*[0-9]\{1,\}\.\([0-9]\{1,\}\)\.[0-9]\{1,\}.*/\1/p')
+        if [ "$major" -gt 3 ]; then
+            return 0
+        fi
+        if [ "$major" -eq 3 ] && [ "$minor" -ge 1 ]; then
+            return 0
+        fi
+        echo "protoc version not match, version 3.1.x is needed, current version: ${version}"
+        return 1
+    else
+        echo "protoc is not installed, version 3.1.x is needed."
+        return 1
     fi
-    if [ "$major" -eq 3 ] && [ "$minor" -ge 1 ]; then
-        return 0
-    fi
-    echo "protoc version not match, version 3.1.x is needed, current version: ${version}"
-    return 1
 }
 
 push () {
