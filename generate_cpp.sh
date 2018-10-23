c6c01e005e0335c422970d964b18cc27fa1cece9
Han Fei <hanfei19910905@gmail.com>
2018-10-23 15:32:02 +0800

    gen cmake file

--- generate_cpp.sh
+++ generate_cpp.sh
@@ -11,4 +11,25 @@ KVPROTO_ROOT=`pwd`
 push proto
 GOGO_ROOT=${KVPROTO_ROOT}/vendor/github.com/gogo/protobuf
 protoc -I.:${GOGO_ROOT}:../include --cpp_out ../cpp *.proto || exit $?
+pop
+
+push cpp
+
+echo "generate CMakeLists..."
+
+CMAKE_FILE=CMakeLists.txt
+
+cat <<EOF > ../${CMAKE_FILE}
+add_library(kvproto
+EOF
+
+for file in `ls`
+    do
+    echo "    cpp/${file}" >> ../${CMAKE_FILE}
+done
+
+cat <<EOF >> ../${CMAKE_FILE}
+)
+target_include_directories (kvproto PUBLIC cpp)
+EOF
 
