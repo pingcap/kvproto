5f327216b946f1c81854792ae445ba1707679bf3
ice1000 <ice1000kotlin@foxmail.com>
2019-01-29 08:03:59 -0500

    Add script for prost generation, replace rust-protobuf with prost
    Signed-off-by: ice1000 <ice1000kotlin@foxmail.com>

---
+++ build.rs
@@ -0,0 +1,85 @@
+extern crate prost_build;
+
+use std::error::Error as StdError;
+use std::io::{Error, ErrorKind, Result, Write};
+use std::{env, fs};
+use std::path::PathBuf;
+
+fn generate_lib_rs(protos: Vec<String>) -> Result<()> {
+    let target: PathBuf =  std::env::current_dir().map(|mut dir| {
+        dir.push("src");
+        dir.push("lib.rs");
+        dir
+    })?;
+    let mut file = fs::File::create(target)?;
+    file.write("extern crate futures;\n".as_bytes())?;
+    file.write("extern crate grpcio;\n".as_bytes())?;
+    file.write("extern crate bytes;\n".as_bytes())?;
+    file.write("extern crate prost;\n".as_bytes())?;
+    file.write("#[macro_use]\n".as_bytes())?;
+    file.write("extern crate prost_derive;\n".as_bytes())?;
+    file.write("extern crate raft;\n".as_bytes())?;
+    file.write("use raft::eraftpb;\n".as_bytes())?;
+    protos.into_iter().map(|s| {
+        let proto_name = s.trim_right_matches(".proto");
+        file.write("pub mod ".as_bytes())?;
+        file.write(proto_name.as_bytes())?;
+        file.write(" { include!(concat!(env!(\"OUT_DIR\"), \"/".as_bytes())?;
+        file.write(proto_name.as_bytes())?;
+        file.write(".rs\")); }\n".as_bytes())?;
+        Ok(())
+    }).for_each(|x: Result<()>| x.unwrap());
+
+    Ok(())
+}
+
+fn generate_import_all_proto() -> Result<(PathBuf, Vec<String>)> {
+      let proto_dir = std::env::current_dir().map(|mut dir| {
+        dir.push("proto");
+        dir
+    })?;
+    let protos: Vec<_> = proto_dir
+        .read_dir()
+        .unwrap()
+        .filter_map(|dir| {
+            let entry = dir.unwrap();
+            if entry.file_type().unwrap().is_file() {
+                entry.file_name().to_str().map(|s| s.to_string())
+            } else {
+                None
+            }
+        })
+        .collect();
+    let mut import_all_path = proto_dir.clone();
+    import_all_path.push("import_all.proto");
+    let mut file = fs::File::create(import_all_path.clone())?;
+    file.write("syntax = \"proto3\";\n".as_bytes())?;
+    file.write("package this_file_is_supposed_to_be_empty;\n".as_bytes())?;
+    protos.iter().map(|name| {
+        file.write("import \"".as_bytes())?;
+        file.write(name.as_bytes())?;
+        file.write("\";\n".as_bytes())?;
+        Ok(())
+    }).for_each(|x: Result<()>| x.unwrap());
+    file.sync_all()?;
+    Ok((import_all_path, protos))
+}
+
+fn main() {
+    let (import_all_path, protos) = generate_import_all_proto().unwrap();
+
+    prost_build::compile_protos(
+        &["proto/import_all.proto"],
+        &["/home/ice1000/git-repos/PingCAP/kvproto/proto",
+          "/home/ice1000/git-repos/PingCAP/kvproto/include",
+          "/home/ice1000/git-repos/PingCAP/kvproto"])
+        .map_err(|err| {
+            println!("{}", err.description());
+            Err::<(), ()>(())
+        })
+        .unwrap();
+    fs::remove_file(import_all_path).unwrap();
+
+    generate_lib_rs(protos).unwrap();
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,19 @@
 extern crate futures;
 extern crate grpcio;
-extern crate protobuf;
+extern crate bytes;
+extern crate prost;
+#[macro_use]
+extern crate prost_derive;
 extern crate raft;
-
 use raft::eraftpb;
-
-pub mod coprocessor;
-pub mod debugpb;
-pub mod debugpb_grpc;
-pub mod errorpb;
-pub mod import_kvpb;
-pub mod import_kvpb_grpc;
-pub mod import_sstpb;
-pub mod import_sstpb_grpc;
-pub mod kvrpcpb;
-pub mod metapb;
-pub mod pdpb;
-pub mod pdpb_grpc;
-pub mod raft_cmdpb;
-pub mod raft_serverpb;
-pub mod tikvpb;
-pub mod tikvpb_grpc;
+pub mod raft_serverpb { include!(concat!(env!("OUT_DIR"), "/raft_serverpb.rs")); }
+pub mod errorpb { include!(concat!(env!("OUT_DIR"), "/errorpb.rs")); }
+pub mod metapb { include!(concat!(env!("OUT_DIR"), "/metapb.rs")); }
+pub mod raft_cmdpb { include!(concat!(env!("OUT_DIR"), "/raft_cmdpb.rs")); }
+pub mod pdpb { include!(concat!(env!("OUT_DIR"), "/pdpb.rs")); }
+pub mod kvrpcpb { include!(concat!(env!("OUT_DIR"), "/kvrpcpb.rs")); }
+pub mod tikvpb { include!(concat!(env!("OUT_DIR"), "/tikvpb.rs")); }
+pub mod debugpb { include!(concat!(env!("OUT_DIR"), "/debugpb.rs")); }
+pub mod coprocessor { include!(concat!(env!("OUT_DIR"), "/coprocessor.rs")); }
+pub mod import_sstpb { include!(concat!(env!("OUT_DIR"), "/import_sstpb.rs")); }
+pub mod import_kvpb { include!(concat!(env!("OUT_DIR"), "/import_kvpb.rs")); }
