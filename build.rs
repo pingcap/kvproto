afef2f72e10d210b4c9aa715c423e777ad6b4d8a
Nick Cameron <nrc@ncameron.org>
2019-02-25 12:16:27 +1300

    Use kvproto-build crate

--- build.rs
+++ build.rs
@@ -11,13 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use regex::Regex;
+use kvproto_build::*;
 use std::env;
 use std::fs::read_dir;
 use std::fs::File;
-use std::io::{Read, Write};
-use std::process::Command;
-use std::str;
+use std::io::Write;
 
 fn main() {
     // This build script creates files in the `src` directory. Since that is
@@ -48,22 +46,16 @@ fn main() {
 
     match BufferLib::from_env_vars() {
         BufferLib::Protobuf => {
-            generate_protobuf_files(file_names);
+            generate_protobuf_files(&file_names, "src/protobuf");
 
-            let mut mod_names: Vec<_> = read_dir("src/protobuf")
-                .expect("Couldn't read src directory")
-                .filter_map(|e| {
-                    let file_name = e.expect("Couldn't list file").file_name();
-                    file_name
-                        .to_string_lossy()
-                        .split(".rs")
-                        .next()
-                        .map(|n| n.to_owned())
-                })
+            let mod_names = module_names_for_dir("src/protobuf");
+
+            let out_file_names: Vec<_> = mod_names
+                .iter()
+                .map(|m| format!("src/protobuf/{}.rs", m))
                 .collect();
-            mod_names.sort();
-
-            replace_read_unknown_fields(&mod_names);
+            let out_file_names: Vec<_> = out_file_names.iter().map(|f| &**f).collect();
+            replace_read_unknown_fields(&out_file_names);
             generate_protobuf_rs(&mod_names);
         }
         BufferLib::Prost => {
@@ -90,72 +82,6 @@ impl BufferLib {
             (Some(_), _) => BufferLib::Prost,
             (_, Some(_)) => BufferLib::Protobuf,
         }
-    }
-}
-
-fn check_protoc_version() {
-    let ver_re = Regex::new(r"([0-9]+)\.([0-9]+)\.[0-9]").unwrap();
-    let ver = Command::new("protoc")
-        .arg("--version")
-        .output()
-        .expect("Program `protoc` not installed (is it in PATH?).");
-    let caps = ver_re
-        .captures(str::from_utf8(&ver.stdout).unwrap())
-        .unwrap();
-    let major = caps.get(1).unwrap().as_str().parse::<i16>().unwrap();
-    let minor = caps.get(2).unwrap().as_str().parse::<i16>().unwrap();
-    if major == 3 && minor < 1 || major < 3 {
-        panic!(
-            "Invalid version of protoc (required 3.1.x, get {}.{}.x).",
-            major, minor,
-        );
-    }
-}
-
-fn generate_protobuf_files(file_names: Vec<&str>) {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/protobuf",
-        input: &file_names,
-        includes: &["proto", "include"],
-        customize: protoc_rust::Customize {
-            ..Default::default()
-        },
-    })
-    .unwrap();
-
-    protoc_grpcio::compile_grpc_protos(file_names, &["proto", "include"], "src/protobuf").unwrap();
-}
-
-// Use the old way to read protobuf enums.
-// FIXME: Remove this once stepancheg/rust-protobuf#233 is resolved.
-fn replace_read_unknown_fields(mod_names: &[String]) {
-    let regex =
-        Regex::new(r"::protobuf::rt::read_proto3_enum_with_unknown_fields_into\(([^,]+), ([^,]+), &mut ([^,]+), [^\)]+\)\?").unwrap();
-    for mod_name in mod_names {
-        let file_name = &format!("src/protobuf/{}.rs", mod_name);
-
-        let mut text = String::new();
-        {
-            let mut f = File::open(file_name).unwrap();
-            f.read_to_string(&mut text)
-                .expect("Couldn't read source file");
-        }
-
-        // FIXME Rustfmt bug in string literals
-        #[rustfmt::skip]
-        let text = {
-            regex.replace_all(
-                &text,
-                "if $1 == ::protobuf::wire_format::WireTypeVarint {\
-                    $3 = $2.read_enum()?;\
-                 } else {\
-                    return ::std::result::Result::Err(::protobuf::rt::unexpected_wire_type(wire_type));\
-                 }",
-            )
-        };
-        let mut out = File::create(file_name).unwrap();
-        out.write_all(text.as_bytes())
-            .expect("Could not write source file");
     }
 }
 
