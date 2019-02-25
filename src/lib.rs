ccdf8527a20b4ef8c7ea530e16165df64b4cadae
Nick Cameron <nrc@ncameron.org>
2019-02-26 10:20:44 +1300

    Add Prost support

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[cfg(feature = "protobuf-codec")]
-mod protobuf;
-
-#[cfg(feature = "protobuf-codec")]
 pub use crate::protobuf::*;
 
 #[cfg(feature = "prost-codec")]
 pub use crate::prost::*;
+
+#[cfg(feature = "prost-codec")]
+mod prost;
+#[cfg(feature = "protobuf-codec")]
+mod protobuf;
