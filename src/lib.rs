61e27e5e76f91c7ea0c95eeea8539b8d73b44576
qupeng <qupeng@pingcap.com>
2019-04-16 13:46:24 +0800

    a little fix.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-mod protobuf;
-
-#[cfg(lib_rust_protobuf)]
-=======
 #[cfg(feature = "protobuf-codec")]
 mod protobuf;
 
 #[cfg(feature = "protobuf-codec")]
->>>>>>> master
 pub use crate::protobuf::*;
 
 #[cfg(feature = "prost-codec")]
