1da1601f6f62d6488386fc4e4bcd145ced1e024a
disksing <i@disksing.com>
2020-03-24 16:25:53 +0800

    rename
    Signed-off-by: disksing <i@disksing.com>

--- pkg/replicate_mode/replicate_mode.pb.go
+++ pkg/replicate_mode/replicate_mode.pb.go
@@ -26,49 +26,49 @@ type ReplicateStatus_Mode int32
 
 const (
 	ReplicateStatus_MAJORITY    ReplicateStatus_Mode = 0
-	ReplicateStatus_MS_AUTOSYNC ReplicateStatus_Mode = 1
+	ReplicateStatus_DR_AUTOSYNC ReplicateStatus_Mode = 1
 )
 
 var ReplicateStatus_Mode_name = map[int32]string{
 	0: "MAJORITY",
-	1: "MS_AUTOSYNC",
+	1: "DR_AUTOSYNC",
 }
 var ReplicateStatus_Mode_value = map[string]int32{
 	"MAJORITY":    0,
-	"MS_AUTOSYNC": 1,
+	"DR_AUTOSYNC": 1,
 }
 
 func (x ReplicateStatus_Mode) String() string {
 	return proto.EnumName(ReplicateStatus_Mode_name, int32(x))
 }
 func (ReplicateStatus_Mode) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{0, 0}
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{0, 0}
 }
 
-type MSAutoSync_State int32
+type DRAutoSync_State int32
 
 const (
-	MSAutoSync_SYNC         MSAutoSync_State = 0
-	MSAutoSync_ASYNC        MSAutoSync_State = 1
-	MSAutoSync_SYNC_RECOVER MSAutoSync_State = 2
+	DRAutoSync_SYNC         DRAutoSync_State = 0
+	DRAutoSync_ASYNC        DRAutoSync_State = 1
+	DRAutoSync_SYNC_RECOVER DRAutoSync_State = 2
 )
 
-var MSAutoSync_State_name = map[int32]string{
+var DRAutoSync_State_name = map[int32]string{
 	0: "SYNC",
 	1: "ASYNC",
 	2: "SYNC_RECOVER",
 }
-var MSAutoSync_State_value = map[string]int32{
+var DRAutoSync_State_value = map[string]int32{
 	"SYNC":         0,
 	"ASYNC":        1,
 	"SYNC_RECOVER": 2,
 }
 
-func (x MSAutoSync_State) String() string {
-	return proto.EnumName(MSAutoSync_State_name, int32(x))
+func (x DRAutoSync_State) String() string {
+	return proto.EnumName(DRAutoSync_State_name, int32(x))
 }
-func (MSAutoSync_State) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{1, 0}
+func (DRAutoSync_State) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{1, 0}
 }
 
 type RegionReplicateStatus_State int32
@@ -91,13 +91,13 @@ func (x RegionReplicateStatus_State) String() string {
 	return proto.EnumName(RegionReplicateStatus_State_name, int32(x))
 }
 func (RegionReplicateStatus_State) EnumDescriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{2, 0}
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{2, 0}
 }
 
 // The replicate status sync from PD to TiKV.
 type ReplicateStatus struct {
 	Mode                 ReplicateStatus_Mode `protobuf:"varint,1,opt,name=mode,proto3,enum=replicate_mode.ReplicateStatus_Mode" json:"mode,omitempty"`
-	MsAutosync           *MSAutoSync          `protobuf:"bytes,2,opt,name=ms_autosync,json=msAutosync" json:"ms_autosync,omitempty"`
+	DrAutosync           *DRAutoSync          `protobuf:"bytes,2,opt,name=dr_autosync,json=drAutosync" json:"dr_autosync,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}             `json:"-"`
 	XXX_unrecognized     []byte               `json:"-"`
 	XXX_sizecache        int32                `json:"-"`
@@ -107,7 +107,7 @@ func (m *ReplicateStatus) Reset()         { *m = ReplicateStatus{} }
 func (m *ReplicateStatus) String() string { return proto.CompactTextString(m) }
 func (*ReplicateStatus) ProtoMessage()    {}
 func (*ReplicateStatus) Descriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{0}
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{0}
 }
 func (m *ReplicateStatus) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -143,17 +143,17 @@ func (m *ReplicateStatus) GetMode() ReplicateStatus_Mode {
 	return ReplicateStatus_MAJORITY
 }
 
-func (m *ReplicateStatus) GetMsAutosync() *MSAutoSync {
+func (m *ReplicateStatus) GetDrAutosync() *DRAutoSync {
 	if m != nil {
-		return m.MsAutosync
+		return m.DrAutosync
 	}
 	return nil
 }
 
-// The status of ms-autosync mode.
-type MSAutoSync struct {
-	Label                string           `protobuf:"bytes,1,opt,name=label,proto3" json:"label,omitempty"`
-	State                MSAutoSync_State `protobuf:"varint,2,opt,name=state,proto3,enum=replicate_mode.MSAutoSync_State" json:"state,omitempty"`
+// The status of dr-autosync mode.
+type DRAutoSync struct {
+	LabelKey             string           `protobuf:"bytes,1,opt,name=label_key,json=labelKey,proto3" json:"label_key,omitempty"`
+	State                DRAutoSync_State `protobuf:"varint,2,opt,name=state,proto3,enum=replicate_mode.DRAutoSync_State" json:"state,omitempty"`
 	RecoverId            uint64           `protobuf:"varint,3,opt,name=recover_id,json=recoverId,proto3" json:"recover_id,omitempty"`
 	WaitSyncTimeoutHint  int32            `protobuf:"varint,4,opt,name=wait_sync_timeout_hint,json=waitSyncTimeoutHint,proto3" json:"wait_sync_timeout_hint,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}         `json:"-"`
@@ -161,18 +161,18 @@ type MSAutoSync struct {
 	XXX_sizecache        int32            `json:"-"`
 }
 
-func (m *MSAutoSync) Reset()         { *m = MSAutoSync{} }
-func (m *MSAutoSync) String() string { return proto.CompactTextString(m) }
-func (*MSAutoSync) ProtoMessage()    {}
-func (*MSAutoSync) Descriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{1}
+func (m *DRAutoSync) Reset()         { *m = DRAutoSync{} }
+func (m *DRAutoSync) String() string { return proto.CompactTextString(m) }
+func (*DRAutoSync) ProtoMessage()    {}
+func (*DRAutoSync) Descriptor() ([]byte, []int) {
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{1}
 }
-func (m *MSAutoSync) XXX_Unmarshal(b []byte) error {
+func (m *DRAutoSync) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-func (m *MSAutoSync) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+func (m *DRAutoSync) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
-		return xxx_messageInfo_MSAutoSync.Marshal(b, m, deterministic)
+		return xxx_messageInfo_DRAutoSync.Marshal(b, m, deterministic)
 	} else {
 		b = b[:cap(b)]
 		n, err := m.MarshalTo(b)
@@ -182,40 +182,40 @@ func (m *MSAutoSync) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return b[:n], nil
 	}
 }
-func (dst *MSAutoSync) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_MSAutoSync.Merge(dst, src)
+func (dst *DRAutoSync) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DRAutoSync.Merge(dst, src)
 }
-func (m *MSAutoSync) XXX_Size() int {
+func (m *DRAutoSync) XXX_Size() int {
 	return m.Size()
 }
-func (m *MSAutoSync) XXX_DiscardUnknown() {
-	xxx_messageInfo_MSAutoSync.DiscardUnknown(m)
+func (m *DRAutoSync) XXX_DiscardUnknown() {
+	xxx_messageInfo_DRAutoSync.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_MSAutoSync proto.InternalMessageInfo
+var xxx_messageInfo_DRAutoSync proto.InternalMessageInfo
 
-func (m *MSAutoSync) GetLabel() string {
+func (m *DRAutoSync) GetLabelKey() string {
 	if m != nil {
-		return m.Label
+		return m.LabelKey
 	}
 	return ""
 }
 
-func (m *MSAutoSync) GetState() MSAutoSync_State {
+func (m *DRAutoSync) GetState() DRAutoSync_State {
 	if m != nil {
 		return m.State
 	}
-	return MSAutoSync_SYNC
+	return DRAutoSync_SYNC
 }
 
-func (m *MSAutoSync) GetRecoverId() uint64 {
+func (m *DRAutoSync) GetRecoverId() uint64 {
 	if m != nil {
 		return m.RecoverId
 	}
 	return 0
 }
 
-func (m *MSAutoSync) GetWaitSyncTimeoutHint() int32 {
+func (m *DRAutoSync) GetWaitSyncTimeoutHint() int32 {
 	if m != nil {
 		return m.WaitSyncTimeoutHint
 	}
@@ -235,7 +235,7 @@ func (m *RegionReplicateStatus) Reset()         { *m = RegionReplicateStatus{} }
 func (m *RegionReplicateStatus) String() string { return proto.CompactTextString(m) }
 func (*RegionReplicateStatus) ProtoMessage()    {}
 func (*RegionReplicateStatus) Descriptor() ([]byte, []int) {
-	return fileDescriptor_replicate_mode_208de959d6e8e3da, []int{2}
+	return fileDescriptor_replicate_mode_42b7bc2451599399, []int{2}
 }
 func (m *RegionReplicateStatus) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -280,10 +280,10 @@ func (m *RegionReplicateStatus) GetRecoverId() uint64 {
 
 func init() {
 	proto.RegisterType((*ReplicateStatus)(nil), "replicate_mode.ReplicateStatus")
-	proto.RegisterType((*MSAutoSync)(nil), "replicate_mode.MSAutoSync")
+	proto.RegisterType((*DRAutoSync)(nil), "replicate_mode.DRAutoSync")
 	proto.RegisterType((*RegionReplicateStatus)(nil), "replicate_mode.RegionReplicateStatus")
 	proto.RegisterEnum("replicate_mode.ReplicateStatus_Mode", ReplicateStatus_Mode_name, ReplicateStatus_Mode_value)
-	proto.RegisterEnum("replicate_mode.MSAutoSync_State", MSAutoSync_State_name, MSAutoSync_State_value)
+	proto.RegisterEnum("replicate_mode.DRAutoSync_State", DRAutoSync_State_name, DRAutoSync_State_value)
 	proto.RegisterEnum("replicate_mode.RegionReplicateStatus_State", RegionReplicateStatus_State_name, RegionReplicateStatus_State_value)
 }
 func (m *ReplicateStatus) Marshal() (dAtA []byte, err error) {
@@ -306,11 +306,11 @@ func (m *ReplicateStatus) MarshalTo(dAtA []byte) (int, error) {
 		i++
 		i = encodeVarintReplicateMode(dAtA, i, uint64(m.Mode))
 	}
-	if m.MsAutosync != nil {
+	if m.DrAutosync != nil {
 		dAtA[i] = 0x12
 		i++
-		i = encodeVarintReplicateMode(dAtA, i, uint64(m.MsAutosync.Size()))
-		n1, err := m.MsAutosync.MarshalTo(dAtA[i:])
+		i = encodeVarintReplicateMode(dAtA, i, uint64(m.DrAutosync.Size()))
+		n1, err := m.DrAutosync.MarshalTo(dAtA[i:])
 		if err != nil {
 			return 0, err
 		}
@@ -322,7 +322,7 @@ func (m *ReplicateStatus) MarshalTo(dAtA []byte) (int, error) {
 	return i, nil
 }
 
-func (m *MSAutoSync) Marshal() (dAtA []byte, err error) {
+func (m *DRAutoSync) Marshal() (dAtA []byte, err error) {
 	size := m.Size()
 	dAtA = make([]byte, size)
 	n, err := m.MarshalTo(dAtA)
@@ -332,16 +332,16 @@ func (m *MSAutoSync) Marshal() (dAtA []byte, err error) {
 	return dAtA[:n], nil
 }
 
-func (m *MSAutoSync) MarshalTo(dAtA []byte) (int, error) {
+func (m *DRAutoSync) MarshalTo(dAtA []byte) (int, error) {
 	var i int
 	_ = i
 	var l int
 	_ = l
-	if len(m.Label) > 0 {
+	if len(m.LabelKey) > 0 {
 		dAtA[i] = 0xa
 		i++
-		i = encodeVarintReplicateMode(dAtA, i, uint64(len(m.Label)))
-		i += copy(dAtA[i:], m.Label)
+		i = encodeVarintReplicateMode(dAtA, i, uint64(len(m.LabelKey)))
+		i += copy(dAtA[i:], m.LabelKey)
 	}
 	if m.State != 0 {
 		dAtA[i] = 0x10
@@ -410,8 +410,8 @@ func (m *ReplicateStatus) Size() (n int) {
 	if m.Mode != 0 {
 		n += 1 + sovReplicateMode(uint64(m.Mode))
 	}
-	if m.MsAutosync != nil {
-		l = m.MsAutosync.Size()
+	if m.DrAutosync != nil {
+		l = m.DrAutosync.Size()
 		n += 1 + l + sovReplicateMode(uint64(l))
 	}
 	if m.XXX_unrecognized != nil {
@@ -420,10 +420,10 @@ func (m *ReplicateStatus) Size() (n int) {
 	return n
 }
 
-func (m *MSAutoSync) Size() (n int) {
+func (m *DRAutoSync) Size() (n int) {
 	var l int
 	_ = l
-	l = len(m.Label)
+	l = len(m.LabelKey)
 	if l > 0 {
 		n += 1 + l + sovReplicateMode(uint64(l))
 	}
@@ -520,7 +520,7 @@ func (m *ReplicateStatus) Unmarshal(dAtA []byte) error {
 			}
 		case 2:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field MsAutosync", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field DrAutosync", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -544,10 +544,10 @@ func (m *ReplicateStatus) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if m.MsAutosync == nil {
-				m.MsAutosync = &MSAutoSync{}
+			if m.DrAutosync == nil {
+				m.DrAutosync = &DRAutoSync{}
 			}
-			if err := m.MsAutosync.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if err := m.DrAutosync.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -573,7 +573,7 @@ func (m *ReplicateStatus) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-func (m *MSAutoSync) Unmarshal(dAtA []byte) error {
+func (m *DRAutoSync) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -596,15 +596,15 @@ func (m *MSAutoSync) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: MSAutoSync: wiretype end group for non-group")
+			return fmt.Errorf("proto: DRAutoSync: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: MSAutoSync: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: DRAutoSync: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Label", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field LabelKey", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -629,7 +629,7 @@ func (m *MSAutoSync) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.Label = string(dAtA[iNdEx:postIndex])
+			m.LabelKey = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
 			if wireType != 0 {
@@ -645,7 +645,7 @@ func (m *MSAutoSync) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.State |= (MSAutoSync_State(b) & 0x7F) << shift
+				m.State |= (DRAutoSync_State(b) & 0x7F) << shift
 				if b < 0x80 {
 					break
 				}
@@ -905,34 +905,34 @@ var (
 )
 
 func init() {
-	proto.RegisterFile("replicate_mode.proto", fileDescriptor_replicate_mode_208de959d6e8e3da)
+	proto.RegisterFile("replicate_mode.proto", fileDescriptor_replicate_mode_42b7bc2451599399)
 }
 
-var fileDescriptor_replicate_mode_208de959d6e8e3da = []byte{
-	// 393 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0x5f, 0xaa, 0xd3, 0x40,
-	0x18, 0xc5, 0x3b, 0x35, 0x91, 0xf6, 0x6b, 0x69, 0xc3, 0x58, 0x25, 0x08, 0x86, 0x10, 0x14, 0x02,
-	0x42, 0x84, 0x16, 0x44, 0xf0, 0x29, 0x2d, 0x41, 0x2b, 0x4d, 0x0b, 0x93, 0x28, 0xf4, 0x69, 0x48,
-	0x93, 0x41, 0x03, 0x4d, 0xa6, 0x24, 0x13, 0xa5, 0x3b, 0x71, 0x0d, 0xbe, 0xb8, 0x0d, 0x1f, 0x5d,
-	0x81, 0x48, 0xdd, 0x88, 0x4c, 0xd2, 0x22, 0x8d, 0xf7, 0xde, 0xb7, 0x7c, 0xff, 0x4e, 0xce, 0x8f,
-	0x33, 0x30, 0x29, 0xd8, 0x61, 0x9f, 0xc6, 0x91, 0x60, 0x34, 0xe3, 0x09, 0x73, 0x0e, 0x05, 0x17,
-	0x1c, 0x8f, 0xae, 0xbb, 0xd6, 0x77, 0x04, 0x63, 0x72, 0x69, 0x05, 0x22, 0x12, 0x55, 0x89, 0x5f,
-	0x81, 0x22, 0x67, 0x3a, 0x32, 0x91, 0x3d, 0x9a, 0x3e, 0x75, 0x5a, 0x42, 0xad, 0x75, 0xc7, 0xe7,
-	0x09, 0x23, 0xf5, 0x05, 0x7e, 0x0d, 0x83, 0xac, 0xa4, 0x51, 0x25, 0x78, 0x79, 0xcc, 0x63, 0xbd,
-	0x6b, 0x22, 0x7b, 0x30, 0x7d, 0xdc, 0x16, 0xf0, 0x03, 0xb7, 0x12, 0x3c, 0x38, 0xe6, 0x31, 0x81,
-	0xac, 0x74, 0xcf, 0xdb, 0xd6, 0x33, 0x50, 0xa4, 0x14, 0x1e, 0x42, 0xcf, 0x77, 0xdf, 0x6d, 0xc8,
-	0x32, 0xdc, 0x6a, 0x1d, 0x3c, 0x86, 0x81, 0x1f, 0x50, 0xf7, 0x7d, 0xb8, 0x09, 0xb6, 0xeb, 0x85,
-	0x86, 0xac, 0x5f, 0x08, 0xe0, 0x9f, 0x02, 0x9e, 0x80, 0xba, 0x8f, 0x76, 0x6c, 0x5f, 0xbb, 0xed,
-	0x93, 0xa6, 0xc0, 0x2f, 0x41, 0x2d, 0x45, 0x24, 0x58, 0x6d, 0x61, 0x34, 0x35, 0x6f, 0xb7, 0xe0,
-	0x48, 0x0a, 0x46, 0x9a, 0x75, 0xfc, 0x04, 0xa0, 0x60, 0x31, 0xff, 0xcc, 0x0a, 0x9a, 0x26, 0xfa,
-	0x3d, 0x13, 0xd9, 0x0a, 0xe9, 0x9f, 0x3b, 0xcb, 0x04, 0xcf, 0xe0, 0xd1, 0x97, 0x28, 0x15, 0x54,
-	0xfa, 0xa5, 0x22, 0xcd, 0x18, 0xaf, 0x04, 0xfd, 0x94, 0xe6, 0x42, 0x57, 0x4c, 0x64, 0xab, 0xe4,
-	0x81, 0x9c, 0x4a, 0xd5, 0xb0, 0x99, 0xbd, 0x4d, 0x73, 0x61, 0x39, 0xa0, 0xd6, 0xff, 0xc0, 0x3d,
-	0x50, 0x6a, 0x86, 0x0e, 0xee, 0x83, 0xea, 0x36, 0x38, 0x58, 0x83, 0xa1, 0xfc, 0xa2, 0xc4, 0x5b,
-	0x6c, 0x3e, 0x78, 0x44, 0xeb, 0x5a, 0xdf, 0x10, 0x3c, 0x24, 0xec, 0x63, 0xca, 0xf3, 0x76, 0x30,
-	0xee, 0x85, 0xaa, 0x49, 0xe6, 0xf9, 0xff, 0xc9, 0xdc, 0x70, 0x75, 0x17, 0x60, 0xb7, 0x05, 0x68,
-	0xbd, 0xb8, 0x78, 0xbd, 0x0e, 0x41, 0x87, 0xc9, 0x72, 0x1d, 0x7a, 0x6f, 0x64, 0x49, 0xa5, 0x4d,
-	0xba, 0x72, 0xe7, 0xde, 0x4a, 0x43, 0x73, 0xed, 0xc7, 0xc9, 0x40, 0x3f, 0x4f, 0x06, 0xfa, 0x7d,
-	0x32, 0xd0, 0xd7, 0x3f, 0x46, 0x67, 0x77, 0xbf, 0x7e, 0x68, 0xb3, 0xbf, 0x01, 0x00, 0x00, 0xff,
-	0xff, 0xd0, 0x02, 0xfc, 0x1f, 0x80, 0x02, 0x00, 0x00,
+var fileDescriptor_replicate_mode_42b7bc2451599399 = []byte{
+	// 400 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x52, 0xdf, 0xea, 0xd3, 0x30,
+	0x18, 0x5d, 0x66, 0x2b, 0xeb, 0xb7, 0xb1, 0x95, 0x38, 0xa5, 0x28, 0x96, 0x52, 0x14, 0x0a, 0x42,
+	0x85, 0x0d, 0x44, 0xf0, 0xaa, 0xdb, 0x8a, 0x4e, 0xf7, 0x07, 0xb2, 0x2a, 0xec, 0x2a, 0x74, 0x6d,
+	0xd0, 0xe2, 0xd6, 0x8c, 0x2e, 0x55, 0xfa, 0x26, 0x3e, 0x83, 0x37, 0xbe, 0x86, 0x97, 0x3e, 0x82,
+	0x4c, 0x7c, 0x0f, 0x49, 0xbb, 0x21, 0xeb, 0xef, 0xcf, 0x5d, 0x72, 0xbe, 0xf3, 0x9d, 0x9c, 0xc3,
+	0x09, 0xf4, 0x33, 0xb6, 0xdf, 0x26, 0x51, 0x28, 0x18, 0xdd, 0xf1, 0x98, 0xb9, 0xfb, 0x8c, 0x0b,
+	0x8e, 0xbb, 0x97, 0xa8, 0xfd, 0x03, 0x41, 0x8f, 0x9c, 0xa1, 0x95, 0x08, 0x45, 0x7e, 0xc0, 0x2f,
+	0x41, 0x91, 0x33, 0x03, 0x59, 0xc8, 0xe9, 0x0e, 0x9e, 0xb8, 0x35, 0xa1, 0x1a, 0xdd, 0x9d, 0xf3,
+	0x98, 0x91, 0x72, 0x03, 0xbf, 0x82, 0x76, 0x9c, 0xd1, 0x30, 0x17, 0xfc, 0x50, 0xa4, 0x91, 0xd1,
+	0xb4, 0x90, 0xd3, 0x1e, 0x3c, 0xac, 0x0b, 0x4c, 0x88, 0x97, 0x0b, 0xbe, 0x2a, 0xd2, 0x88, 0x40,
+	0x9c, 0x79, 0x27, 0xb6, 0xfd, 0x14, 0x14, 0x29, 0x85, 0x3b, 0xd0, 0x9a, 0x7b, 0x6f, 0x97, 0x64,
+	0x1a, 0xac, 0xf5, 0x06, 0xee, 0x41, 0x7b, 0x42, 0xa8, 0xf7, 0x3e, 0x58, 0xae, 0xd6, 0x8b, 0xb1,
+	0x8e, 0xec, 0xbf, 0x08, 0xe0, 0xbf, 0x02, 0x7e, 0x04, 0xda, 0x36, 0xdc, 0xb0, 0x2d, 0xfd, 0xcc,
+	0x8a, 0xd2, 0xb1, 0x46, 0x5a, 0x25, 0xf0, 0x8e, 0x15, 0xf8, 0x05, 0xa8, 0x07, 0x11, 0x0a, 0x56,
+	0x3a, 0xe9, 0x0e, 0xac, 0x9b, 0x9d, 0xb8, 0x32, 0x0c, 0x23, 0x15, 0x1d, 0x3f, 0x06, 0xc8, 0x58,
+	0xc4, 0xbf, 0xb0, 0x8c, 0x26, 0xb1, 0x71, 0xc7, 0x42, 0x8e, 0x42, 0xb4, 0x13, 0x32, 0x8d, 0xf1,
+	0x10, 0x1e, 0x7c, 0x0d, 0x13, 0x41, 0xa5, 0x6d, 0x2a, 0x92, 0x1d, 0xe3, 0xb9, 0xa0, 0x9f, 0x92,
+	0x54, 0x18, 0x8a, 0x85, 0x1c, 0x95, 0xdc, 0x93, 0x53, 0xa9, 0x1a, 0x54, 0xb3, 0x37, 0x49, 0x2a,
+	0x6c, 0x17, 0xd4, 0xf2, 0x0d, 0xdc, 0x02, 0xa5, 0x8c, 0xd2, 0xc0, 0x1a, 0xa8, 0x5e, 0x95, 0x0a,
+	0xeb, 0xd0, 0x91, 0x27, 0x4a, 0xfc, 0xf1, 0xf2, 0x83, 0x4f, 0xf4, 0xa6, 0xfd, 0x1d, 0xc1, 0x7d,
+	0xc2, 0x3e, 0x26, 0x3c, 0xad, 0xf7, 0xe3, 0x9d, 0x53, 0x55, 0x05, 0x3d, 0xbb, 0x5a, 0xd0, 0x35,
+	0x5b, 0xb7, 0x05, 0x6c, 0xd6, 0x02, 0xda, 0xcf, 0xcf, 0x5e, 0x2f, 0xbb, 0x30, 0xa0, 0x3f, 0x5d,
+	0x04, 0xfe, 0x6b, 0x79, 0xa5, 0xd2, 0x26, 0x9d, 0x79, 0x23, 0x7f, 0xa6, 0xa3, 0x91, 0xfe, 0xf3,
+	0x68, 0xa2, 0x5f, 0x47, 0x13, 0xfd, 0x3e, 0x9a, 0xe8, 0xdb, 0x1f, 0xb3, 0xb1, 0xb9, 0x5b, 0xfe,
+	0xb7, 0xe1, 0xbf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x81, 0x59, 0x60, 0xdb, 0x87, 0x02, 0x00, 0x00,
 }
