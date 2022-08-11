b9bee046f655b1827ebfda8d6912ea1313ffd15c
D3Hunter <jujj603@gmail.com>
2022-08-11 15:13:03 +0800

    fix comments

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-extra_arg=""
-if [ "$(uname -p)" == "arm" ]; then
-	extra_arg="--platform linux/x86_64"
-fi
-
-docker build $extra_arg . -t tikv/kvproto:3.8.0
+docker build . -t tikv/kvproto:3.8.0
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-extra_arg=""
-if [ "$(uname -p)" == "arm" ]; then
-	extra_arg="--platform linux/x86_64"
-fi
-
-docker run $extra_arg --rm -i -t -v "$(pwd):$(pwd)" -w "$(pwd)" tikv/kvproto:3.8.0 "$@"
-
+docker run --rm -i -t -v "$(pwd):$(pwd)" -w "$(pwd)" tikv/kvproto:3.8.0 "$@"
